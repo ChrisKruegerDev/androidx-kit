6c1785466a70f86e81e90892adafab482f1e277e
chris <15275971+chrisbln@users.noreply.github.com>
2020-06-04 18:22:32 +0200

    Update live data

--- library/src/main/java/com/placebox/androidx/lifecycle/ListLiveDataExt.kt
+++ library/src/main/java/com/placebox/androidx/lifecycle/ListLiveDataExt.kt
@@ -2,19 +2,14 @@ package com.placebox.androidx.lifecycle
 
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
 import com.placebox.androidx.widget.recyclerview.adapter.AbstractListRecyclerAdapter
+import com.placebox.ktx.lifecycle.bind
 
 fun <T : Any> LiveData<out List<T>>.bindAdapterUpdate(owner: LifecycleOwner, adapter: AbstractListRecyclerAdapter<T>) {
-    observe(owner, Observer {
-        if (adapter.data.isEmpty())
-            adapter.setData(it)
-        else
-            adapter.updateData(it)
-    })
+    bind(owner) { if (adapter.data.isEmpty()) adapter.setData(it) else adapter.updateData(it) }
 }
 
 fun <T : Any> LiveData<out List<T>>.bindAdapter(owner: LifecycleOwner, adapter: AbstractListRecyclerAdapter<T>) {
-    observe(owner, Observer { adapter.setData(it) })
+    bind(owner) { adapter.setData(it) }
 }
 
