fefaac4215d6bf9fcfb4a4470b0b2ba7d7adaa78
chris <15275971+chrisbln@users.noreply.github.com>
2020-10-14 18:12:03 +0200

    Extend AppBar on change

--- library/src/main/java/com/placebox/androidx/widget/appbar/TitleAppBarChangeListener.kt
+++ library/src/main/java/com/placebox/androidx/widget/appbar/TitleAppBarChangeListener.kt
@@ -1,15 +1,15 @@
 package com.placebox.androidx.widget.appbar
 
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.appbar.AppBarLayout
 
 class TitleAppBarChangeListener(
-    private val activity: AppCompatActivity,
     threshold: Int,
+    private val activity: AppCompatActivity,
     private val title: String
 ) : AppBarStateChangeListener(threshold) {
 
-    override fun onStateChanged(layout: AppBarLayout?, state: State?) {
+    override fun onStateChanged(state: State) {
         activity.supportActionBar?.title = if (state == State.COLLAPSED) title else null
     }
+
 }
