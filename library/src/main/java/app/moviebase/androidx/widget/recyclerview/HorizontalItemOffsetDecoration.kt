5ac86499597fd62b90fba61ee5b9db7f4bbb5ea0
Christian Krüger <15275971+chrisnkrueger@users.noreply.github.com>
2022-05-28 10:01:42 +0200

    Add horizontal decorator

---
+++ library/src/main/java/app/moviebase/androidx/widget/recyclerview/HorizontalItemOffsetDecoration.kt
@@ -0,0 +1,18 @@
+package app.moviebase.androidx.widget.recyclerview
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import app.moviebase.ktx.content.px
+
+class HorizontalItemOffsetDecoration(
+    offsetDp: Int,
+) : RecyclerView.ItemDecoration() {
+
+    private val offset = offsetDp.px
+
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        super.getItemOffsets(outRect, view, parent, state)
+        outRect.right = offset
+    }
+}
