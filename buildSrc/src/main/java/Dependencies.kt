ea92ab977aab06048a91b64da2a5fd03bef0db98
Christian Krüger <15275971+chrisnkrueger@users.noreply.github.com>
2022-06-13 08:51:15 +0200

    Upgrade libraries

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -12,7 +12,7 @@ object Versions {
     val buildTools = "31.0.0"
 
     // Plugins
-    val androidGradle = "7.1.3"
+    val androidGradle = "7.2.1"
     val androidJunit = "1.7.1.1"
     val dokka = "1.4.30"
     val nexus = "1.0.0"
@@ -20,24 +20,24 @@ object Versions {
 
     // Kotlin
     val kotlin = "1.6.21"
-    val coroutines = "1.6.1"
+    val coroutines = "1.6.2"
 
     // AndroidX
-    val appCompat = "1.4.1"
-    val core = "1.7.0"
+    val appCompat = "1.4.2"
+    val core = "1.8.0"
     val constraintLayout = "2.1.3"
     val preference = "1.2.0"
     val recyclerView = "1.2.1"
-    val paging3 = "3.1.0-alpha02"
+    val paging3 = "3.1.1"
     val browser = "1.4.0"
     val collection = "1.1.0"
     val webkit = "1.4.0"
 
     // Google
-    val material = "1.6.0"
+    val material = "1.6.1"
 
     // UI
-    val glide = "4.13.1"
+    val glide = "4.13.2"
 
     // Util
     val androidKtx = "1.4.1"
@@ -46,7 +46,7 @@ object Versions {
     // Testing
     val junitJupiter = "5.8.2"
     val truth = "1.1.3"
-    val mockitoInline = "4.5.1"
+    val mockitoInline = "4.6.1"
     val mockitoKotlin = "4.0.0"
 }
 
