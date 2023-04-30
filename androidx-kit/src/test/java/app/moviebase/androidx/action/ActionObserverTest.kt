0535d3df7e5de036ff08955ced95a1855bad550f
Christian Krueger <15275971+chrisnkrueger@users.noreply.github.com>
2023-04-30 11:07:51 +0200

    Add unit test

---
+++ androidx-kit/src/test/java/app/moviebase/androidx/action/ActionObserverTest.kt
@@ -0,0 +1,12 @@
+package app.moviebase.androidx.action
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class ActionObserverTest {
+
+    @Test
+    fun `example test`() {
+        assertTrue(true)
+    }
+}
